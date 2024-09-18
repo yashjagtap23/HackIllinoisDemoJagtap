d5a42d236265d309380b0b30051b822cb0b91525
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-09-17 20:39:12 -0500

    Initial Commit

---
+++ HackIllinoisEvent/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HackIllinoisEvent
+//
+//  Created by Yash Jagtap on 9/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HackIllinoisEvent/HackIllinoisEventApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackIllinoisEventApp.swift
+//  HackIllinoisEvent
+//
+//  Created by Yash Jagtap on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HackIllinoisEventApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
